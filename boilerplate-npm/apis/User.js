02b76f696984fcd26bf194c95134a682bb8d295b
anandsinghyadav <anandyadav2332@gmail.com>
2023-12-25 22:27:22 +0530

    Post request

--- boilerplate-npm/apis/User.js
+++ boilerplate-npm/apis/User.js
@@ -5,5 +5,12 @@ route.get("/users" , (req,res) =>{
     res.status(201).send("data sending")
 })
 
+route.send("/users" , (req, res) =>{
+    let body = req.body
+    if(body){
+        console.log("Got the data")
+        res.status(201).send("getting the data")
+    }
+})
 
 module.exports = route
